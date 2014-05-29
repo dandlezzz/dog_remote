fc62d87496ad27aed8c37b6cb4a2b4210522caab
Dandlezzz <danieladammiller@gmail.com>
2014-05-28 21:45:20 -0700

    initial commit

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,21 @@
+class RequestsController < ApplicationController
+	def new
+		@request = Request.new
+	end
+
+	def create
+		Request.create(request_params)
+		redirect_to new_request_path
+	end
+
+	def index
+		respond_to  do |f|
+			f.json{ render json: Request.last} 
+		end	
+	end
+
+	private
+	def request_params
+		params.require(:request).permit(:name)
+	end
+end
